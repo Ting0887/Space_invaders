59f334f707a79c8e28dbf35ec9a6365c93440b70
Ting0887 <lutingyang@gmail.com>
2022-10-23 16:27:00 +0800

    update code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ canvas.width = 600;
 canvas.height = 600;
 
 const background = new Image();
-background.src = "images/space.png";
+background.src = "images/space.jpg";
 
 const playerBulletController = new BulletController(canvas, 10, "red", true);
 const enemyBulletController = new BulletController(canvas, 4, "white", false);
@@ -18,6 +18,10 @@ const player = new Player(canvas, 3, playerBulletController);
 
 let isGameOver = false;
 let didwin = false;
+let GameWin = new Audio("sounds/gamewin.wav");
+GameWin.volume = 1;
+let GameLose = new Audio("sounds/gameover.wav");
+GameLose.volume = 1;
 
 function game(){
     checkGameOver();
@@ -31,6 +35,7 @@ function game(){
     }
 }
 
+// 勝利或失敗顯示在遊戲畫面上
 function displayGameOver(){
     if(isGameOver){
         let text = didwin ? "You Win" : "Game Over";
@@ -46,15 +51,23 @@ function checkGameOver(){
     if(isGameOver){
         return;
     }
+    //如果玩家被子彈擊中
     if(enemyBulletController.collideWith(player)){
         isGameOver = true;
+        GameLose.currentTime = 0;
+        GameLose.play();
     }
+    //如果玩家碰到敵人
     if(enemyController.collideWith(player)){
         isGameOver = true;
+        GameLose.currentTime = 0;
+        GameLose.play();
     }
     if(enemyController.enemyRows.length === 0){
         didwin = true;
         isGameOver = true;
+        GameWin.currentTime = 0;
+        GameWin.play();
     }
 }
 setInterval(game, 1000/60);
